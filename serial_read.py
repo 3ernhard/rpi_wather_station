b2c8af4a793e12277cc74387f5ab99fd38f127fa
Bernhard <bernhard@tp-x395>
2020-06-11 00:14:20 +0200

    serial exceptions

--- serial_read.py
+++ serial_read.py
@@ -3,16 +3,15 @@
 import serial
 
 
-previous_reading = -999
+previous_reading = -999.9
 
 
 def read_celsius(port='/dev/ttyACM0', baudrate=9600, timeout=1):
     global preprevious_reading
     try:
         reading = float(serial.Serial(**locals()).readline())
-    except serial.serialutil.SerialException:
+    except:
         reading = previous_reading
-        print()
     prereading = previous_reading
     return reading
 
