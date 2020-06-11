0b2750373adcdbcffc4502e67eb920a16a9a8c10
Bernhard <bernhard@tp-x395>
2020-06-11 08:50:00 +0200

    serial transactions

--- AnalogRead/AnalogRead.ino
+++ AnalogRead/AnalogRead.ino
@@ -1,20 +1,17 @@
-#define PIN	0
-#define BAUD	9600
-#define WAIT	1000
-
-
-void print_celsius() {
-    float k {log(10000.0 * ((1024.0 / analogRead(PIN) - 1)))};
-    Serial.println((1 / (0.001129148 + (0.000234125 + (0.0000000876741 * k * k)) * k)) - 273.15);
-}
+#define PIN     0
+#define BAUD    9600
+#define WAIT    1000
 
 
 void setup() {
-	Serial.begin(BAUD);
+    Serial.begin(BAUD);
 }
 
 
 void loop() {
-	print_celsius();
-	delay(WAIT);
+    if (Serial.read() >= 0) {
+        float k = log(10000.0 * ((1024.0 / analogRead(PIN) - 1)));
+        float c = (1 / (0.001129148 + (0.000234125 + (0.0000000876741 * k * k)) * k)) - 273.15;
+        Serial.println(c);
+    }
 }
--- serial_read.py
+++ serial_read.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
 import serial
+import time
 
 
-previous_reading = -999.9
-
-
-def read_celsius(port='/dev/ttyACM0', baudrate=9600, timeout=1):
-    global preprevious_reading
-    try:
-        reading = float(serial.Serial(**locals()).readline())
-    except:
-        reading = previous_reading
-    prereading = previous_reading
-    return reading
+def read_celsius(port='/dev/ttyACM0', baudrate=9600):
+    while True:
+        try:
+            s = serial.Serial(port, baudrate)
+            s.write(1)
+            return float(s.readline())
+        except serial.serialutil.SerialException:
+            time.sleep(0.1)
+            continue
+        except ValueError:
+            print("No data.")
+            time.sleep(5)
 
 
 if __name__ == '__main__':
