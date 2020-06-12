af638296895ae2d0c662893994467f343938e1c9
Bernhard <bernhard@tp-x395>
2020-06-12 22:26:32 +0200

    changed to median

--- weather_station.py
+++ weather_station.py
@@ -3,6 +3,7 @@
 from datetime import datetime
 from time import sleep
 import os
+import numpy as np
 
 from BME280 import BME280
 from serial_read import read_celsius
@@ -25,23 +26,47 @@ with open(F, "w") as csv:
     csv.write(f"#{time_str},°C,°C,hPa,%\n")
 
 
+i = 0
 # build average over BIN_SIZE, record every second
-i = t_outside = t_inside = pressure = humidity = 0
+# t_outside = t_inside = pressure = humidity = 0
+# build median over BIN_SIZE, record every second
+t_outside = []
+t_inside = []
+pressure = []
+humidity = []
+
 while sensor.refresh():
 
-    t_outside += read_celsius()
-    t_inside  += sensor.temperature
-    pressure  += sensor.pressure
-    humidity  += sensor.humidity
+    # t_outside += read_celsius()
+    # t_inside += sensor.temperature
+    # pressure += sensor.pressure
+    # humidity += sensor.humidity
+    t_outside.append(read_celsius())
+    t_inside.append(sensor.temperature)
+    pressure.append(sensor.pressure)
+    humidity.append(sensor.humidity)
 
     if i == BIN_SIZE:
-        t_outside /= BIN_SIZE
-        t_inside  /= BIN_SIZE
-        pressure  /= BIN_SIZE
-        humidity  /= BIN_SIZE
+
+        # t_outside /= BIN_SIZE
+        # t_inside /= BIN_SIZE
+        # pressure /= BIN_SIZE
+        # humidity /= BIN_SIZE
+        t_outside = np.median(t_outside)
+        t_inside = np.median(t_inside)
+        pressure = np.median(pressure)
+        humidity = np.median(humidity)
+
         with open(F, "a") as csv:
             csv.write(datetime.now().strftime(f"{time_str},{t_outside:g},{t_inside:g},{pressure:g},{humidity:g}\n"))
-        i = t_outside = t_inside = pressure = humidity = 0
+
+        i = 0
+        # t_outside = t_inside = pressure = humidity = 0
+        t_outside = []
+        t_inside = []
+        pressure = []
+        humidity = []
+
     else:
         i += 1
 
