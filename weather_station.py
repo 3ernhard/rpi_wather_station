7e6916382da51cab24cf94c269a251141082ffb0
Bernhard <bernhard@tp-x395>
2020-07-01 20:41:46 +0200

    toggle LED based on inside > outside temp

--- weather_station.py
+++ weather_station.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 from time import sleep
+from subprocess import call
 import os
 import numpy as np
 
@@ -14,13 +15,40 @@ STEP_SIZE = 10
 # in seconds (int)
 METHOD = np.median
 
+LED = "OFF"
+
 
 sensor = BME280()
 time_str = "%Y-%m-%dT%H:%M:%S"
-F = os.path.dirname(os.path.realpath(__file__))
+file_path = os.path.dirname(os.path.realpath(__file__))
+F = file_path
 F += "/data/"
 F += datetime.now().strftime(time_str.replace(":", "-"))
 F += ".csv"
+
+
+def go_dark(force=False):
+    """ Turn of RPI LEDs"""
+    global LED
+    if force or LED != "OFF" :
+        call(file_path+"/go-dark")
+        LED = "OFF"
+
+
+def go_green(force=False):
+    """ Turn on RPI's green LED"""
+    global LED
+    if force or LED != "GREEN":
+        call(file_path+"/go-green")
+        LED = "GREEN"
+
+
+def go_red(force=False):
+    """ Turn on RPI's red LED"""
+    global LED
+    if force or LED != "RED" :
+        call(file_path+"/go-red")
+        LED = "RED"
 
 
 with open(F, "w") as csv:
@@ -28,37 +56,49 @@ with open(F, "w") as csv:
     csv.write(f"#{time_str},°C,°C,hPa,%\n")
 
 
-i = 0
-# build METHOD over BIN_SIZE, record every STEP_SIZE seconds
-t_outside = []
-t_inside = []
-pressure = []
-humidity = []
+if __name__ == '__main__':
 
-while sensor.refresh():
+    go_dark(force=True)
 
-    t_outside.append(read_celsius())
-    t_inside.append(sensor.temperature)
-    pressure.append(sensor.pressure)
-    humidity.append(sensor.humidity)
+    i = 0
+    # build METHOD over BIN_SIZE, record every STEP_SIZE seconds
+    t_outside = []
+    t_inside = []
+    pressure = []
+    humidity = []
 
-    if i >= BIN_SIZE:
+    while sensor.refresh():
 
-        t_outside = METHOD(t_outside)
-        t_inside = METHOD(t_inside)
-        pressure = METHOD(pressure)
-        humidity = METHOD(humidity)
+        t_out = read_celsius()
+        t_in = sensor.temperature
 
-        with open(F, "a") as csv:
-            csv.write(datetime.now().strftime(f"{time_str},{t_outside:g},{t_inside:g},{pressure:g},{humidity:g}\n"))
+        if t_in > t_out:
+            go_green()
+        else:
+            go_dark()
 
-        i = 0
-        t_outside = []
-        t_inside = []
-        pressure = []
-        humidity = []
+        t_outside.append(t_out)
+        t_inside.append(t_in)
+        pressure.append(sensor.pressure)
+        humidity.append(sensor.humidity)
 
-    else:
-        i += STEP_SIZE
+        if i >= BIN_SIZE:
 
-    sleep(STEP_SIZE)
+            t_outside = METHOD(t_outside)
+            t_inside = METHOD(t_inside)
+            pressure = METHOD(pressure)
+            humidity = METHOD(humidity)
+
+            with open(F, "a") as csv:
+                csv.write(datetime.now().strftime(f"{time_str},{t_outside:g},{t_inside:g},{pressure:g},{humidity:g}\n"))
+
+            i = 0
+            t_outside = []
+            t_inside = []
+            pressure = []
+            humidity = []
+
+        else:
+            i += STEP_SIZE
+
+        sleep(STEP_SIZE)
