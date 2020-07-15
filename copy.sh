df87a0431bdc76dff4f99d00082b95b2e88438c5
Bernhard <bernhard@tp-x395>
2020-07-15 19:41:46 +0200

    fixed path

--- copy.sh
+++ copy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -avz pi4:/home/pi/weather_station/data/*.csv /home/bernhard/Documents/RPI/weather_station/data/
+rsync -avz pi4:/home/pi/weather_station/data/*.csv /home/bernhard/Projects/RPI/weather_station/data/
