25675fd395949aba9639f2a3ee4970cad11c633f
Bernhard <bernhard@tp-x395>
2020-08-31 18:40:28 +0200

    copy

--- copy.sh
+++ copy.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-rsync -avz -- pi:/home/pi/weather_station/data/*.csv /home/bernhard/Projects/RPI/weather_station/data/
+# https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# is a useful one-liner which will give you the full directory name of the script no matter where it is being called from.
+
+rsync -avz -- pi:/home/pi/weather_station/data/*.csv "$DIR/data/"
+
+unset DIR
